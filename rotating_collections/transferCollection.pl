6493d5e44d995060dd49d3ae90260536bf91d9cd
wabbit <johncarlo2695@gmail.com>
2017-08-08 21:12:51 +0800

    Initial Commit

---
+++ rotating_collections/transferCollection.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+#
+
+use Modern::Perl;
+
+use C4::Output;
+use C4::Auth;
+use C4::Context;
+use C4::RotatingCollections;
+
+use CGI qw ( -utf8 );
+
+my $query = new CGI;
+
+my $colId    = $query->param('colId');
+my $toBranch = $query->param('toBranch');
+
+my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
+    {
+        template_name   => "rotating_collections/transferCollection.tt",
+        query           => $query,
+        type            => "intranet",
+        authnotrequired => 0,
+        flagsrequired   => { tools => 'rotating_collections' },
+        debug           => 1,
+    }
+);
+
+## Transfer collection
+my ( $success, $errorCode, $errorMessage );
+if ($toBranch) {
+    ( $success, $errorCode, $errorMessage ) =
+      TransferCollection( $colId, $toBranch );
+
+    if ($success) {
+        $template->param( transferSuccess => 1 );
+    }
+    else {
+        $template->param(
+            transferFailure => 1,
+            errorCode       => $errorCode,
+            errorMessage    => $errorMessage
+        );
+    }
+}
+
+## Get data about collection
+my ( $colTitle, $colDesc, $colBranchcode );
+( $colId, $colTitle, $colDesc, $colBranchcode ) = GetCollection($colId);
+$template->param(
+    colId            => $colId,
+    colTitle         => $colTitle,
+    colDesc          => $colDesc,
+    colBranchcode    => $colBranchcode,
+);
+
+output_html_with_http_headers $query, $cookie, $template->output;
